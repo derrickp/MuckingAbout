dd2509d68610576af79c283ba5017b405f6d7333
Derrick Plotsky <derrickplotsky@gmail.com>
2018-01-08 17:55:51 -0800

    Initial Commit

---
+++ MuckingAbout/Views/AboutPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace MuckingAbout
+{
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
